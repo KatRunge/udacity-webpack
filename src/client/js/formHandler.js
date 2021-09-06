d1fbc4088171d7049d635dcfb6adcd32adcda6f1
Katja <katja.runge@jimdo.com>
2021-09-06 20:55:36 +0200

    feat: add post method

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,29 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+  // check what text was put into the form field
+  let inputUrl = document.getElementById("input").value;
+  Client.checkForName(inputUrl);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  fetch("http://localhost:8081/testApi", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(inputUrl),
+  })
+    .then((res) => res.json())
+    .then(function (res) {
+      try {
+        document.getElementById("score").innerHTML = res.score_tag;
+        document.getElementById("agreement").innerHTML = res.agreement;
+        document.getElementById("subjectivity").innerHTML = res.subjectivity;
+        document.getElementById("confidence").innerHTML = res.confidence;
+      } catch (error) {
+        console.log("error", error);
+      }
+    });
+  console.log('öasldkfj aösldkjf ', inputUrl);
+  console.log("::: Form Submitted :::");
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,71 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-require('dotenv').config()
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+require("dotenv").config();
 
-const apikey = process.env.API_KEY
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
+const apiKey = process.env.API_KEY;
 
+// console.log(apiUrl);
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('dist/index.html'))
-})
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+  //   res.sendFile(path.resolve("dist/index.html"));
+});
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+  console.log("Example app listening on port 8081!");
+});
 
-app.get('/test', function (req, res) {
-    res.json(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.json(mockAPIResponse);
+});
+
+// app.post("/testApi", async function (req, res) {
+//   const inputUrl = req.body.inputUrl;
+
+//   const data = await getApiURL(inputUrl);
+//   res.json(data);
+// });
+
+// async function getApiURL(inputUrl) {
+//   const apiURL = `${baseURL}?key=${apiKey}&of=json&url=${inputUrl}&lang=auto`;
+//   console.log(apiURL);
+//   try {
+//     const res = await fetch(apiURL);
+//     const data = await res.json();
+
+//     return {
+//       score_tag: data.score_tag,
+//       agreement: data.agreement,
+//       subjectivity: data.subjectivity,
+//       confidence: data.confidence,
+//     };
+//   } catch (err) {
+//     console.log("Error:", err);
+//   }
+// }
+
+app.post("/testApi", async (req, res) => {
+  const inputUrl = req.body.inputUrl;
+  const url = await fetch(
+    `${baseURL}?key=${apiKey}&url=${inputUrl}&lang=auto&of=json`
+  );
+  console.log(url);
+  try {
+    const data = await url.json();
+    res.send(data);
+  } catch (error) {
+    console.log("error", error);
+  }
+});
